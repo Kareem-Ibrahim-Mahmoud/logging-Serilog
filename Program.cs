be5b4857f655b78a1baa2e2f9f63ea5b24a24989
kareem ibrahim <kareemimrahim@gmail.com>
2024-09-14 01:16:36 +0300

    The Progect Serilog forloop

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_one_c_sharp
+{
+
+
+    internal class emp
+    {
+
+
+        static void Main(string[] args)
+        {
+            /*
+            Log.Logger = new LoggerConfiguration()
+          .WriteTo.Console()  
+          .CreateLogger();
+            */
+
+
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+           .WriteTo.File(@"D:\c sharp prog\Serilog and nlog\Serilog.txt", rollingInterval: RollingInterval.Infinite) 
+           .CreateLogger();
+
+            // بدء حلقة for لتسجيل 100,000 عملية
+            for (int i = 1; i <= 100000; i++)
+            {
+                Log.Information("Iteration {i} of the loop", i);  // تسجيل قيمة i
+            }
+
+            // إنهاء Serilog بشكل صحيح للتأكد من كتابة جميع الـ logs
+            Log.CloseAndFlush();
+
+            Console.WriteLine("Logging completed. Check the logs file.");
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}       
